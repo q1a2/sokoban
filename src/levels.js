0fa52620e85c8239beb14cf096d0d5bc397e5d26
Kevin W <kevwonnacott1@gmail.com>
2021-03-20 20:07:42 -0600

    Added More levels

--- src/levels.js
+++ src/levels.js
@@ -9,7 +9,7 @@ function generateLevel(tiles) {
 let levels = [
   {
     id: 1,
-    name: "Simple",
+    name: "Start",
     minMoves: 6,
     creator: "default",
     tiles: generateLevel([
@@ -22,6 +22,86 @@ let levels = [
           "_","_","W","W","W","W","W","_","_",
           "_","_","_","_","_","_","_","_","_",
           "_","_","_","_","_","_","_","_","_"])
+  },
+  {
+    id: 2,
+    name: "Staircase",
+    minMoves: 29,
+    creator: "default",
+    tiles: generateLevel([
+          "W","W","W","W","W","W","W","W","W",
+          "W","W","W","W","W","W","W","W","W",
+          "W","W","_","_","_","W","W","W","W",
+          "W","W","P","B","B","_","W","W","W",
+          "W","W","W","_","_","_","W","W","W",
+          "W","W","W","W","_","H","W","W","W",
+          "W","W","W","W","W","H","W","W","W",
+          "W","W","W","W","W","F","W","W","W",
+          "W","W","W","W","W","W","W","W","W"])
+  },
+  {
+    id: 3,
+    name: "Switch Sides",
+    minMoves: 50,
+    creator: "default",
+    tiles: generateLevel([
+          "W","W","W","W","W","W","W","W","W",
+          "W","W","W","W","W","W","W","W","W",
+          "W","_","_","W","W","W","W","W","W",
+          "W","_","_","B","_","W","W","F","W",
+          "W","_","_","B","P","W","W","_","W",
+          "W","_","_","B","_","H","H","H","W",
+          "W","W","W","W","W","W","W","W","W",
+          "W","W","W","W","W","W","W","W","W",
+          "W","W","W","W","W","W","W","W","W"])
+  },
+  {
+    id: 4,
+    name: "Double Push",
+    minMoves: 35,
+    creator: "default",
+    tiles: generateLevel([
+          "W","W","W","W","W","W","W","W","W",
+          "W","_","_","_","_","_","W","W","W",
+          "W","_","_","_","W","P","_","W","W",
+          "W","W","_","_","_","B","_","W","W",
+          "W","W","_","_","W","B","W","W","W",
+          "W","W","W","W","W","H","W","W","W",
+          "W","W","W","W","W","H","W","W","W",
+          "W","W","W","W","W","F","W","W","W",
+          "W","W","W","W","W","W","W","W","W"])
+  },
+  {
+    id: 5,
+    name: "Triple Feat",
+    minMoves: 92,
+    creator: "default",
+    tiles: generateLevel([
+          "W","W","W","W","W","W","W","W","W",
+          "W","W","W","W","W","W","W","W","W",
+          "W","_","_","_","_","W","_","_","W",
+          "W","_","B","B","_","P","_","_","W",
+          "W","_","B","_","W","_","_","_","W",
+          "W","_","_","_","W","H","W","_","W",
+          "W","W","W","W","H","H","_","_","W",
+          "W","W","W","W","F","W","W","W","W",
+          "W","W","W","W","W","W","W","W","W"])
+  },
+  {
+    id: 6,
+    name: "Blocked In",
+    minMoves: 55,
+    creator: "default",
+    tiles: generateLevel([
+          "W","W","W","W","W","W","W","W","W",
+          "W","P","_","_","H","H","_","F","W",
+          "W","_","B","B","W","W","W","W","W",
+          "W","W","W","_","W","W","W","W","W",
+          "W","_","_","_","W","W","W","W","W",
+          "W","_","W","_","_","_","W","W","W",
+          "W","_","_","_","W","_","W","W","W",
+          "W","W","W","_","_","_","W","W","W",
+          "W","W","W","W","W","W","W","W","W"])
   }
 ]
 
